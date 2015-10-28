7e1d56ef9d61df257bfacf62763d09f3755fb436
Royce Harding <royceclrw@gmail.com>
2015-10-27 22:09:19 -0400

    add webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
 var path    = require('path');
 
 module.exports = {
-    context : path.resolve('js'),
+    context : path.resolve('public/js'),
     entry   : ['./app'],
     output  : {
         path       : path.resolve('build/'),
         publicPath : '/public/assets/',
         filename   : 'bundle.js'
     },
-
     devServer : {
         contentBase : 'public'
     },
 
-    watch  : true,
     module : {
         loaders : [
             {
                 test    : /\.css$/,
                 exclude : /node_modules/,
                 loader  : 'style-loader!css-loader'
-            }, {
-                test    : /\.(png|jpg|ttf|eot)$/,
-                exclude : /node_modules/,
-                loader  : 'url-loader?limit=10000'
             }
         ]
     },
+
     resolve : {
         extensions : ['', '.js', '.es6']
     }
