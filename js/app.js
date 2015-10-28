1f78d1421480252c7f54adf6315a690ce522978e
Royce Harding <royceclrw@gmail.com>
2015-10-27 23:59:11 -0400

    webpack with react

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
-console.log('App loaded!');
 
-$('#testDiv').text('jQuery modified this content (see app.js)');
+var React = require('react');
 
-require('./login');
+var Timer = React.createClass({
+
+  // some es6 magic. Will be compiled.
+  getInitialState () {
+    return {secondsElapsed : 0};
+  },
+
+  // normal es5  stuff.
+  tick : function () {
+    this.setState({secondsElapsed : this.state.secondsElapsed + 1});
+  },
+
+  componentDidMount : function () {
+    this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentWillUnmount : function () {
+    clearInterval(this.interval);
+  },
+
+  render : function () {
+    // JSX in here gets parsed with the babel loader.
+    return (
+      <div>
+        <h3>It's the final countdown</h3>
+        Seconds Elapsed: {this.state.secondsElapsed}
+      </div>
+    );
+  }
+
+});
+
+React.render(<Timer />, document.getElementById('container'));
